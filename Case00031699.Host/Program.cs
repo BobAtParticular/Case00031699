d67a366d5c0347e837cc33cafc7d0f3d1fd618d8
Bob Langley <bob.langley@particular.net>
2017-10-13 11:51:49 -0700

    Reproduction / sample finished

---
+++ Case00031699.Host/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.Logging;
+using NServiceBus.Persistence;
+using NServiceBus.Persistence.Legacy;
+using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+namespace Case00031699
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	ServiceBus().GetAwaiter().GetResult();
+        }
+
+        static async Task ServiceBus()
+        {
+		    var config = new EndpointConfiguration("Case00031699");
+		    config.SendFailedMessagesTo("Case00031699.Error");
+		    config.AuditProcessedMessagesTo("Case00031699.Audit");
+		    var transport = config.UseTransport<MsmqTransport>();
+		    transport.Transactions(TransportTransactionMode.TransactionScope);
+            transport.Routing().RegisterPublisher(typeof(PcAddedEventV1), "Case00031699");
+
+		    config.AutoSubscribe();
+
+            config.UsePersistence<InMemoryPersistence>();
+            config.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
+
+		    config.DisableFeature<TimeoutManager>();
+		    config.UseSerialization<JsonSerializer>();
+		    var bus = await Endpoint.Start(config);
+
+            Console.WriteLine("Wait until the subscription is received and then press enter to publish");
+            Console.ReadLine();
+
+            await bus.Publish(new PcAddedEventV1());
+
+            Console.WriteLine("Wait until the published event is processed and then press enter to exit");
+            Console.ReadLine();
+
+            await bus.Stop();
+        }
+
+        public class PcAddedEventV1 : IEvent
+        { }
+
+        public class PcAddedEventV1Handler : IHandleMessages<PcAddedEventV1>
+        {
+            static ILog Logger = LogManager.GetLogger<PcAddedEventV1Handler>();
+            public Task Handle(PcAddedEventV1 message, IMessageHandlerContext context)
+            {
+                Logger.Info("PcAddedEventV1 event processed");
+                return Task.FromResult(0);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Case00031699
 Case 00031699 Reproduction
+
+## Reproduction
+
+For MSMQ to subscribe it must know which endpoint publishes the message :
+
+```csharp
+transport.Routing().RegisterPublisher(typeof(PcAddedEventV1), "Case00031699");
+```
+This will tell the endpoint where to send the subscription message to.
+
+In a simple example like this, where a `Publish` is called immediately after startup, a race condition can occur where the subscription message sent above is not received and processed before the publish is sent. 
+
+We can alleviate this by making sure the subscription message is processed before the `Publish` is made with a simple pause, in this case a `Console.Readline` prompting the user to wait until the subscription is processed before proceeding.
+
+In production you will need to configure a durable subscription persistence for MSMQ subscriptions:
+
+```csharp
+config.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
+```
+
+This means on subsequent startups the publisher will _already_ have the subscription stored.
+
+## Production Code
+
+In production you may _still_ need to deal with this potential race condition immediately after deployment, otherwise message publishes may not be sent if the subscriptions have not yet been processed.
+
+In this scenario you will want to _preload_ the subscriptions in the persistence of your choice. For instance, in the case of SQL persistence, this would mean creating a script that is run as part of deployment which already has the necessary subscription entries.
+
+Generating those scripts by querying the persistence in lower environments is usually the easiest approach. 
